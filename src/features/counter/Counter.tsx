02bead8f453fa3a290c318eaa64668d4bcbad265
Yang Sun <sunyang9876@hotmail.com>
2023-08-11 21:51:55 +1000

    1-complete

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,17 +1,38 @@
 import React from "react";
 
-import { increment, decrement } from "./counterSlice";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 
 const Counter: React.FC = () => {
+  const [incrementAmount, setIncrementAmount] = React.useState<string>(0);
+
+  const addValue = Number(incrementAmount) || 0;
   const count = useAppSelector((state) => state.counter.count);
   const dispatch = useAppDispatch();
+
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
   return (
     <section>
       <p>{count}</p>
       <div>
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+
+      <input
+        type="text"
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>
+          Add Amount
+        </button>
+        <button onClick={resetAll}>Reset</button>
       </div>
     </section>
   );
