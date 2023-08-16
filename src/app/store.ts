ea54f991b97a3a64dd9971cbd020c6fba17053f8
Yang Sun <sunyang9876@hotmail.com>
2023-08-16 21:09:52 +1000

    6-RTK-query completed

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,13 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import postsReducer from "../features/posts/postsSlice";
 import usersReducer from "../features/users/usersSlice";
+import { apiSlice } from "../features/api/apiSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     posts: postsReducer,
     users: usersReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
---
+++ src/features/todos/TodoList.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import {
+  useGetTodosQuery,
+  useAddTodoMutation,
+  useUpdateTodoMutation,
+  useDeleteTodoMutation,
+} from "../api/apiSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
+import { v4 as uuidv4 } from "uuid";
+
+const TodoList: React.FC = () => {
+  const [newTodo, setNewTodo] = React.useState<string>("");
+
+  const {
+    data: todos,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetTodosQuery(undefined);
+  const [addTodo] = useAddTodoMutation();
+  const [updateTodo] = useUpdateTodoMutation();
+  const [deleteTodo] = useDeleteTodoMutation();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    addTodo({ userId: 1, title: newTodo, completed: false });
+    setNewTodo("");
+  };
+
+  const newItemSection = (
+    <form onSubmit={handleSubmit} className="addForm">
+      <label htmlFor="new-todo">Enter a new todo item</label>
+      <div className="new-todo">
+        <input
+          type="text"
+          id="new-todo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Enter new todo"
+        />
+      </div>
+      <button className="submit">
+        <FontAwesomeIcon icon={faUpload} />
+      </button>
+    </form>
+  );
+
+  let content;
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } else if (isSuccess) {
+    content = todos.map((todo) => {
+      return (
+        <article key={uuidv4()} className="todoContainer">
+          <div className="todo">
+            <input
+              type="checkbox"
+              name={todo.id.toLocaleString()}
+              id={todo.id.toLocaleString()}
+              checked={todo.completed}
+              onChange={() =>
+                updateTodo({ ...todo, completed: !todo.completed })
+              }
+            />
+            <label htmlFor={todo.id.toLocaleString()}>{todo.title}</label>
+          </div>
+          <button className="trash" onClick={() => deleteTodo({ id: todo.id })}>
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        </article>
+      );
+    });
+  } else if (isError) {
+    content = JSON.stringify(error);
+  }
+
+  return (
+    <main>
+      <h1>Todo List</h1>
+      {newItemSection}
+      {content}
+    </main>
+  );
+};
+
+export default TodoList;
